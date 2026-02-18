46cc8f4799d935a4cf97b10d4c79a6dfce89cb69
yokoarteaga <139139794+yokoarteaga@users.noreply.github.com>
2026-02-18 19:22:04 +0100

    Update and rename preprocessBNF.m to generate_w.m

--- generate_w.m
+++ generate_w.m
@@ -1,4 +1,4 @@
-% pre-process BNF data
+% generate factor w for flatfielding
 clear all, close all, clc
 %% open cube, flat field white, dark 
 [CUBE_1 bands hcube] = openHSI;
@@ -7,8 +7,8 @@ clear all, close all, clc
 [dkwtCUBE bands hcube] = openHSI;
 %[wtsCUBE bands hcube] = openHSI; % open camera white reference
 dims = size(CUBE_1);
-%load rad_whitecc_avg.mat % only load if calculating w
-load w_92.mat
+load rad_whitecc_avg.mat % load white reference radiance
+
 
 %% subtract dark noise
 dkFRAME = repmat(mean(dkCUBE,1),dims(1),1,1);
@@ -17,26 +17,14 @@ dkwtFRAME = repmat(mean(dkwtCUBE,1),dims(1),1,1);
 pixel_im = CUBE_1 - dkFRAME;
 pixel_white = wtCUBE - dkwtFRAME;
 
-%% Y luminance as the peak of the green channel of the white patch 
+% Y luminance as the peak of the green channel of the white patch 
 % average bands 62 to 80 of white patch
 
 Y = mean(rad_whitecc_avg(:,62:80));
 
+% select average area of white patch
 w = Y * (mean(mean(mean(CUBE_1(342:350,209:218,62:80))))./mean(mean(mean(wtCUBE(342:350,209:218,62:80)))));
-%% flatfield correction
-pixel_im_ff = (w / 1000) * pixel_im ./ pixel_white;
 
-%% sRGB image generation
-dims = size(CUBE_1);
-    %illuminant = cat(2,lam,spec');
-    ref_ff = reshape(pixel_im_ff, [dims(1)*dims(2) dims(3)]);
-    ref_interp = interp1(bands,ref_ff',410:5:700)';
-    system = 1931;
-    out_space = 'XYZ';
-    wlength = [410 5 700];
-    illuminant = 'D65';
-    out = t_spd2sth(ref_interp',wlength,illuminant,system,out_space);
-    out_sRGB = xyz2rgb(out'./100, 'whitepoint', 'd65');
     sRGB_IMG = reshape(out_sRGB,[dims(1) dims(2) 3]);
 
   figure,imshow(sRGB_IMG, [], 'InitialMagnification', 'fit');
