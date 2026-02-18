1da90cb3b5fbaae27d4a14cbc72425f497fc3170
yokoarteaga <139139794+yokoarteaga@users.noreply.github.com>
2026-02-18 19:19:31 +0100

    Update and rename preprocessBNF.m to flatfield.m

--- flatfield.m
+++ flatfield.m
@@ -1,4 +1,4 @@
-% pre-process BNF data
+% flatfield hyperspectral cube
 clear all, close all, clc
 %% open cube, flat field white, dark 
 [CUBE_1 bands hcube] = openHSI;
@@ -7,8 +7,8 @@ clear all, close all, clc
 [dkwtCUBE bands hcube] = openHSI;
 %[wtsCUBE bands hcube] = openHSI; % open camera white reference
 dims = size(CUBE_1);
-%load rad_whitecc_avg.mat % only load if calculating w
-load w_92.mat
+
+load w.mat % use create_w.m to generate this factor
 
 %% subtract dark noise
 dkFRAME = repmat(mean(dkCUBE,1),dims(1),1,1);
@@ -17,12 +17,6 @@ dkwtFRAME = repmat(mean(dkwtCUBE,1),dims(1),1,1);
 pixel_im = CUBE_1 - dkFRAME;
 pixel_white = wtCUBE - dkwtFRAME;
 
-%% Y luminance as the peak of the green channel of the white patch 
-% average bands 62 to 80 of white patch
-
-Y = mean(rad_whitecc_avg(:,62:80));
-
-w = Y * (mean(mean(mean(CUBE_1(342:350,209:218,62:80))))./mean(mean(mean(wtCUBE(342:350,209:218,62:80)))));
 %% flatfield correction
 pixel_im_ff = (w / 1000) * pixel_im ./ pixel_white;
 
